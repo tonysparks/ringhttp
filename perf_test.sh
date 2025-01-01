f91b6a4eefca2c839b862310ef12abb96424fd2f
tonysparks <anthony.sparks31@gmail.com>
2025-01-01 12:04:59 -0600

    Clean up QueryParams

--- perf_test.sh
+++ perf_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+valgrind --tool=callgrind ./bin/ring
+
+wrk -t12 -c200 -d30s http://localhost:8080/echo?hello=world
+
 ./bombardier --connections=50 -l -k -n 1000 http://localhost:8080/static/index.html
 
 
